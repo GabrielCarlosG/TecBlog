2d265bd777dc5b260ea818cbc7dfcf24d9db351e
Carlos Gabriel <gabrielcarlos331@gmail.com>
2025-05-09 13:32:21 -0300

    delete cgtech blog

--- CGTECH/blog/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Postagem
-# Create your views here.
-
-def listar_postagens(request):
-    postagens = Postagem.objets.all()
-    return render(request, 'blog/listar_postagens.html', {'postagens':postagens})
