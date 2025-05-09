5be2bc1a40bee60bfbed2cbda0ad3f7581856678
Carlos Gabriel <gabrielcarlos331@gmail.com>
2025-05-09 16:45:42 -0300

    alteração fale conosco

--- APP/__init__.py
+++ APP/__init__.py
@@ -1,10 +1,14 @@
 # app/__init__.py
 from flask import Flask
+from flask_mail import Mail
 
 app = Flask(__name__)
 
 # Carregar configurações (se houver um arquivo config.py)
 app.config.from_object('config.Config')  # Caso tenha uma classe Config em config.py
+
+# Inicializa o Flask-Mail
+mail = Mail(app)
 
 # Importar as rotas
 from app import routes
--- APP/routes.py
+++ APP/routes.py
@@ -1,13 +1,29 @@
 # app/routes.py
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from app import app
 
 @app.route('/', endpoint='index')
 def index():
     return render_template('index.html')
 
-@app.route('/fale-conosco', endpoint='fale_conosco')
+@app.route('/fale-conosco', methods=['GET', 'POST'])
 def fale_conosco():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        assunto = request.form['assunto']
+        mensagem = request.form['mensagem']
+
+        # Aqui, você pode salvar os dados no banco de dados ou enviar um e-mail, por exemplo.
+        # Vou apenas exibir os dados no terminal para teste
+        print(f"Nome: {nome}")
+        print(f"Email: {email}")
+        print(f"Assunto: {assunto}")
+        print(f"Mensagem: {mensagem}")
+
+        # Após processar, podemos redirecionar para a página ou mostrar uma mensagem de sucesso
+        return render_template('fale-conosco.html', mensagem_enviada=True)
+
     return render_template('fale-conosco.html')
 
 @app.route('/sobre', endpoint='sobre')
