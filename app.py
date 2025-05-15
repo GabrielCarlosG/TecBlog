8d9efbe2e0e77ae86523d71ed9f94ba56650515d
Carlos Gabriel <gabrielcarlos331@gmail.com>
2025-05-14 22:06:44 -0300

    Alteração

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from app import  create_app  # Importa a instância Flask do app/__init__.py
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,7 @@
 from app import create_app
+from waitress import serve
 
 app = create_app()
+
+if __name__ == '__main__':
+    serve(app, host='0.0.0.0', port=8000)
