ddda97cfbc2b3dcd706fe2a1c531ec8eb056c675
Carlos Gabriel <gabrielcarlos331@gmail.com>
2025-05-20 17:17:37 -0300

    Criação do db

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 # app.py
 from myapp import  create_app  # Importa a instância Flask do app/__init__.py
+from myapp import db
+from myapp.models import Mensagem
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
     app.run(debug=False)
+
